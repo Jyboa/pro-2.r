03e8cfd3f24575e35188bb8e3a38cf377480ae4d
Jyboa <73274102+Jyboa@users.noreply.github.com>
2020-12-07 09:41:37 +0100

    Add files via upload

---
+++ ProOpgave2.r.js
@@ -0,0 +1,19 @@
+let bet = document.getElementById("bet").value;
+let coin =parseFloat(document.getElementById("coin").value);
+
+
+function roulette (){
+    
+    let bet = document.getElementById("bet").value;
+    let coin =parseFloat(document.getElementById("coin").value);
+    document.getElementById("myElement").innerHTML="bet: "+bet+"<br>coin:"+coin;
+
+    
+} 
+myElement.onclick = roulette
+
+function RouletteRul (){
+    let Resultat = (coin)
+    console.log(coin)
+    return(coin)
+}
